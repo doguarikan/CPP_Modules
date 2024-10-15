14f06f8072776c9725d3a2749046970e37f0362c
Doğu Arıkan <118825959+doguarikan@users.noreply.github.com>
2024-10-16 00:52:32 +0300

    main added

---
+++ CPP00/ex01/main.cpp
@@ -0,0 +1,21 @@
+int main(int argc, char **argv) {
+    std::string input;
+    while(true) {
+        
+        std::cout << "-ADD-" << std::endl;
+        std::cout << "-SEARCH-" << std::endl;
+        std::cout << "-EXIT-" << std::endl;
+        
+        std::cin << input;
+
+        if(input == "ADD") {
+
+        }
+        else if(input == "SEARCH") {
+            
+        }
+        else if(input == "EXIT") {
+            
+        }
+    }
+}
